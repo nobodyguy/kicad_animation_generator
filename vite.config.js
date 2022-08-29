2dc0bfc3af406b8423159e358f6ba229267ce7b8
nobodyguy <4577368+nobodyguy@users.noreply.github.com>
2022-08-29 20:33:44 +0200

    Fixed base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/kicad_animation_generator/",
   server: {
     port: 3000,
   }
